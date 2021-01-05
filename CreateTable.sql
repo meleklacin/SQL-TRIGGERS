850a2c8faf9eba4284e1a563b2997000024d7bac
Melek laçin <meleklacinn@gmail.com>
2021-01-05 10:29:10 +0300

    Update CreateTable.sql

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,5 +1,6 @@
 CREATE TABLE Workers (wID int, wName varchar(100))
  
 CREATE TABLE infworker(wID int, entireDATE DATETIME);
+---Triggerlarda kullanacağımız tablolarımız                                             
 
 
